7d12f5bbb4d435f6125606b75836e8657bde3e33
erdelf <erdelf.productions@gmail.com>
2017-07-06 08:48:22 +0200

    updated imports

--- Source/RimCoin/RimCoin/Building_Computer.cs
+++ Source/RimCoin/RimCoin/Building_Computer.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Verse;
 using RimWorld;
 using UnityEngine;
 using Verse.AI;
-
+    
 namespace RimCoin
 {
     public class Building_Computer : Building
